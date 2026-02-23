5617361345bbd4694c27a59388d5c37b6b944746
0xzoowa <omedeomozuwa@gmail>
2026-02-23 12:47:51 +0100

    newline terminal support

--- kernel.c
+++ kernel.c
@@ -89,6 +89,19 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void terminal_putchar(char c)
 {
+
+    if (c == '\n')
+    {
+        terminal_column = 0;
+        terminal_row++;
+
+        if (terminal_row == VGA_HEIGHT)
+        {
+            terminal_row = 0;
+        }
+
+        return;
+    }
     terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
     if (++terminal_column == VGA_WIDTH)
     {
@@ -115,5 +128,6 @@ void kernel_main(void)
     terminal_initialize();
 
     /* Newline support is left as an exercise. */
-    terminal_writestring("Hello, kernel World!\n");
+    terminal_writestring("Hello\n");
+    terminal_writestring("kernel World!\n"); //"Hello, kernel World!o
 }
